b8bfffb01c75077255876a7fa7076d873a3e3e58
Nik Lussy <nik-lussy@hotmail.com>
2026-02-16 20:16:26 +0100

    dnc

--- Grundlagen/Beispiele/dynamic/scripts.js
+++ Grundlagen/Beispiele/dynamic/scripts.js
@@ -1,4 +1,19 @@
-function changeText() {
-    const introParagraph = document.getElementById('intro');
-    introParagraph.textContent = "Ich bin ein sehr erfahrener Architekt und Software-Entwickler ich schaue auf über 20 Jahre Erfahrung in diversen Technologien und Branchen zurück.";
+﻿function showHobbies() {
+    const hobbiesOutput = document.getElementById("hobbies-output");
+    if (hobbiesOutput.innerHTML.trim() !== "") {
+        hobbiesOutput.innerHTML = "";
+        return;
+    }
+
+    hobbiesOutput.innerHTML = "<strong>Meine Hobbies:</strong> <ul><li>Wintersport</li><li>Kochen</li><li>Entwicklung</li></ul>";
+}
+
+function showContactInfo() {
+    const contactOutput = document.getElementById("contact-output");
+    if (contactOutput.innerHTML.trim() !== "") {
+        contactOutput.innerHTML = "";
+        return;
+    }
+
+    contactOutput.innerHTML = "<strong>Kontakt:</strong> <ul><li>Telefon: <a href=\"tel:+41791234567\">+41 79 123 45 67</a></li><li>E-Mail: <a href=\"mailto:nik.lussy@example.com\">nik.lussy@example.com</a></li></ul>";
 }
